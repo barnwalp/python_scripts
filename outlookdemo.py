c2011cefbb4aa0cf93ef5bc0b1e0baa961bdb2e3
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-06-14 15:42:34 +0530

    feat/fetching sensitive information from enviornment variables

--- outlookdemo.py
+++ outlookdemo.py
@@ -1,10 +1,20 @@
 from exchangelib import Credentials, Account
 from exchangelib import DELEGATE, Configuration
+import os
 
-credentials = Credentials(username='ioc\\00504802', password='*********')
-config = Configuration(server='mail.indianoil.in',
+# getting the sensitive information from enviornment variables
+# stored in the .bash_profile
+outlook_user = os.environ.get('OUTLOOK_USER')
+outlook_password = os.environ.get('OUTLOOK_PASS')
+outlook_server = os.environ.get('OUTLOOK_SERVER')
+outlook_email = os.environ.get('OUTLOOK_EMAIL')
+
+# Using the necessary credential and configuration to connect to
+# the exchange server
+credentials = Credentials(username=outlook_user, password=outlook_password)
+config = Configuration(server=outlook_server,
                        credentials=credentials)
-account = Account(primary_smtp_address='barnwalp@indianoil.in',
+account = Account(primary_smtp_address=outlook_email,
                   config=config, autodiscover=False,
                   access_type=DELEGATE)
 
