786f4e6ac0cb81358057efc1d359d50aadd61044
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-06-19 08:49:15 +0530

    feat/added the html message and mail was triggered

--- outlook_html_table.py
+++ outlook_html_table.py
@@ -17,3 +17,40 @@ config = Configuration(server=outlook_server,
 account = Account(primary_smtp_address=outlook_email,
                   config=config, autodiscover=False,
                   access_type=DELEGATE)
+
+# Getting the excel worsheet for fetching the data
+wb = load_workbook(filename="eRACTS Complaint.xlsx")
+ws = wb["mail"]
+
+mail_body = HTMLBody(
+    """
+    <html>
+        <body>
+            <h3>
+                Pl find the current list of eRACTS complaint which have not
+                yet been resolved, pl go through each case and provide current
+                status of complaint and target date of rectification:
+            </h3>
+            <table>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+        </body>
+    </html>
+    """
+)
+
+msg = Message(
+    account=account,
+    subject="eRACTS complaint status - 190620 - Sambalpur DO",
+    body = mail_body,
+    to_recipients=['barnwalp@indianoil.in']
+)
+msg.send_and_save()
