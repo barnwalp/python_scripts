067d9c58f020238d253d341bc84c0131e3b8d314
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-06-22 21:33:33 +0530

    feat/creating html table using vanilla python

--- html_table.py
+++ html_table.py
@@ -5,55 +5,27 @@ from openpyxl import load_workbook
 wb = load_workbook(filename="eRACTS Complaint.xlsx")
 ws = wb["mail"]
 
-# traverse cells in a spreadsheet column
-# for cell in ws['B']:
-    # print(cell.value)
+# contains content in HTML format
+html = """
+<html><body><h1>Pl find the current list of eRACTS complaint which have not yet been resolved, pl go through each case and provide current status of complaint and target date of rectification:</h1>
+"""
 
 for whole_row in ws.iter_rows(min_row=1, max_row=1):
+    html += "<table border=1><tr>"
     for row in whole_row:
-        print(row.value)
+        html += f'<th>{row.value}</th>'
+    html += "</tr>"
 
-def wrap(a, tag):
-    "Wraps in <td> tag the a"
-    tag1 = tag
-    if tag == "table":
-        tag1 = "table border=1"
-    if tag == "td" and a.strip().replace(".", "").isdigit():
-        print(a, "is a number")
-        tag1 = "td style=\"text-align:right\""
-    return f"<{tag1}>{a}</{tag}>"
+for whole_row in ws.iter_rows(min_row=2):
+    html += "<tr>"
+    for row in whole_row:
+        html += f'<td>{row.value}</td>'
+    html += "</tr>"
 
-
-def split(tab):
-    "Splits a multiline string in a list of items divided by comma for line"
-    tab = tab.splitlines()
-    for n, row in enumerate(tab):
-        tab[n] = row.split(",")
-    return tab
-
-
-def table(tab):
-    html = ''  # contain HTML
-    for n, x in enumerate(tab):
-        print(f'line is: {x}')
-        for a in x:
-            print(f'word is: {a}')
-            html += "<tr>"
-    html = wrap(html, "table")
-    return html
-
-
-data = """
-Impiegato, Performance, data
-Rossi Mario, 1000, 1/2/2018
-Baldo Franco, 2000, 1/2/2018
-"""
-data = data[1:-1]
-print(data)
-
-data = table(data)
+html += "</table></body></html>"
+# print(html)
 
 with open("Simple.html", "w", encoding="utf-8") as filehtml:
-    filehtml.write(data)
+    filehtml.write(html)
 
 os.system("Simple.html")
