722db495cdb8dab04749c38de2874cf257b1bb88
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-06-22 07:54:28 +0530

    feat/traversing rows and column of spredsheet

--- html_table.py
+++ html_table.py
@@ -1,6 +1,17 @@
-# from random import randrange
 import os
+from openpyxl import load_workbook
 
+
+wb = load_workbook(filename="eRACTS Complaint.xlsx")
+ws = wb["mail"]
+
+# traverse cells in a spreadsheet column
+# for cell in ws['B']:
+    # print(cell.value)
+
+for whole_row in ws.iter_rows(min_row=1, max_row=1):
+    for row in whole_row:
+        print(row.value)
 
 def wrap(a, tag):
     "Wraps in <td> tag the a"
@@ -24,20 +35,23 @@ def split(tab):
 def table(tab):
     html = ''  # contain HTML
     for n, x in enumerate(tab):
+        print(f'line is: {x}')
         for a in x:
+            print(f'word is: {a}')
             html += "<tr>"
     html = wrap(html, "table")
     return html
 
 
-data = table(split(
-    """
-    Impiegato, Performance, data
-    Rossi Mario, 1000, 1/2/2018
-    Baldo Franco, 2000, 1/2/2018
-    """
-)[1:-1])
+data = """
+Impiegato, Performance, data
+Rossi Mario, 1000, 1/2/2018
+Baldo Franco, 2000, 1/2/2018
+"""
+data = data[1:-1]
 print(data)
+
+data = table(data)
 
 with open("Simple.html", "w", encoding="utf-8") as filehtml:
     filehtml.write(data)
