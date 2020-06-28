3fe5159df2b6ed9227d13041afebbecfebfd6ba9
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-06-28 17:35:08 +0530

    feat/working with large dataset

--- pandas_stats_excel.py
+++ pandas_stats_excel.py
@@ -12,3 +12,12 @@ df['count'] = 1
 print(df.columns.values)
 print(df.groupby(['Type 1']).count()['count'])
 print(df.groupby(['Type 1', 'Type 2']).count()['count'])
+
+# Working with large amount of data
+new_df = pd.DataFrame(columns=df.columns)
+
+for df in pd.read_csv('modified.csv', chunksize=5):
+    results = df.groupby(['Type 1']).count()
+    new_df = pd.concat([new_df, results])
+
+print(new_df)
