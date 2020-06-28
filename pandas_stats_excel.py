5721f78122293d806d75e9fa13d85a6000c9b8a2
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-06-28 17:26:45 +0530

    feat/groupby functionality in pandas

---
+++ pandas_stats_excel.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+df = pd.read_excel('modified.xlsx')
+
+# Following line will group by the value of Type 1 and provide the mean value
+# for its data; you may also sort the resulting table
+# print(df.groupby(['Type 1']).mean().sort_values('Attack', ascending=False))
+
+# print(df.groupby(['Type 1']).count())
+df['count'] = 1
+print(df.columns.values)
+print(df.groupby(['Type 1']).count()['count'])
+print(df.groupby(['Type 1', 'Type 2']).count()['count'])
