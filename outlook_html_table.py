7f3e9fe21a0202ecb341fd915a48bf1bbbaccd69
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-07-02 10:16:59 +0530

    feat/working with windows downloads folder instead of current working directory

--- outlook_html_table.py
+++ outlook_html_table.py
@@ -17,20 +17,39 @@ account = Account(primary_smtp_address=outlook_email,
                   config=config, autodiscover=False,
                   access_type=DELEGATE)
 
-wb = load_workbook(filename="eRACTS Complaints 230620.xlsx")
+path = '/mnt/c/Users/panka/Downloads/eRACTS_Complaints.xlsx'
+wb = load_workbook(path)
 
 
 def create_mail_table(wb):
-    ws = wb.active
-    ws.unmerge_cells('A1:AI1')
-    ws.unmerge_cells('A2:AI2')
-    ws.delete_rows(1, 2)
+    sheet = wb.active
+    # print(sheet.title)
 
-    ws.delete_cols(21, 13)
-    ws.delete_cols(12, 3)
+    # openpyxl can not copy sheet with merged cells, first all such cells
+    # need to be unmerged and deleted
+    # Delete first two merged rows
+    sheet.unmerge_cells('A1:AI1')
+    sheet.unmerge_cells('A2:AI2')
+    sheet.delete_rows(1, 2)
 
-    ws.insert_cols(19)
-    ws['S1'].value = "Detailed complaint"
+    # Create copy mail for alteration
+    ws = wb.copy_worksheet(sheet)
+    ws.title = "mail"
+    print(ws.title)
+    ws = wb["mail"]
+
+    # Delete column U:AH
+    ws.delete_cols(21, 14)
+    # Delete column P:Q
+    ws.delete_cols(16, 2)
+    # Delete column H:N
+    ws.delete_cols(8, 7)
+    # Delete column D:E
+    ws.delete_cols(4, 2)
+
+    # add a row before column 10
+    ws.insert_cols(10)
+    ws['J1'].value = "Detailed complaint"
 
     # creating a function for filter to filter out None and int types
 
@@ -41,45 +60,37 @@ def create_mail_table(wb):
         else:
             return True
 
-    for cell in ws['S']:
-        value_1 = f'P{cell.row}'
-        value_2 = f'Q{cell.row}'
-        value_3 = f'R{cell.row}'
+    for cell in ws['J']:
+        value_1 = f'G{cell.row}'
+        value_2 = f'H{cell.row}'
+        value_3 = f'I{cell.row}'
         merge_list = [ws[value_1].value,
                       ws[value_2].value,
                       ws[value_3].value
                       ]
-    # filter will return only those value which are neither None nor int
-    cell.value = ', '.join(filter(is_not_str, merge_list))
+        # filter will return only those value which are neither None nor int
+        cell.value = ', '.join(filter(is_not_str, merge_list))
 
-    # copy entire worksheet for mail
-    ws2 = wb.copy_worksheet(ws)
-    ws2.title = "mail"
-
-    ws2 = wb["mail"]
-    ws2.delete_cols(13, 6)
-    ws2.delete_cols(8, 4)
-    ws2.delete_cols(4, 2)
+    # Deleting column G:I
+    ws.delete_cols(7, 3)
 
     filter_value = {'Auto-Closed for Dealer Late Acknowledgement',
                     'Service Completed',
                     'Auto-Closed on Alarm Normalization'
                     }
     # Deleting all complaints which have been closed
-    for cell in ws2['H']:
+    for cell in ws['H']:
         if cell.row > 1:
             if cell.value in filter_value:
                 print(cell.value)
-                ws2.delete_rows(cell.row)
-    ws2.delete_cols(8)
-    ws2['H1'] = "Current Status of complaint"
-    ws2['I1'] = "Target Date"
+                ws.delete_rows(cell.row)
+    # Delete end column H
+    ws.delete_cols(8)
+    ws['H1'] = "Current Status of complaint"
+    ws['I1'] = "Target Date"
 
-    wb.save('eRACTS Complaint.xlsx')
+    wb.save(path)
     return wb
-
-
-wb = create_mail_table(wb)
 
 
 def create_html(wb):
@@ -119,9 +130,10 @@ def create_html(wb):
     return html
 
 
+wb = create_mail_table(wb)
 msg = Message(
     account=account,
-    subject="eRACTS complaint status - 230620 - Sambalpur DO",
+    subject="eRACTS complaint status - Sambalpur DO",
     body=HTMLBody(create_html(wb)),
     to_recipients=['barnwalp@indianoil.in']
 )
