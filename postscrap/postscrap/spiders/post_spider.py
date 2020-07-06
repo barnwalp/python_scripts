3af8d776c54eb61bcb5c409e1a585042538d8966
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-07-06 08:06:13 +0530

    feat/created PostSpider class

--- postscrap/postscrap/spiders/post_spider.py
+++ postscrap/postscrap/spiders/post_spider.py
@@ -1,1 +1,19 @@
 import scrapy
+
+
+class PostSpider(scrapy.Spider):
+    # name is a string to identify spider
+    name = 'posts'
+
+    start_urls = [
+        'https://blog.scrapinghub.com/page/1/',
+        'https://blog.scrapinghub.com/page/2/'
+    ]
+    # default callback to process donwload responses when their
+    # request dont specifies callback
+
+    def parse(self, response):
+        page = response.url.split('/')[-1]
+        filename = 'posts-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
