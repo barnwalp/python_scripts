ee1b411ddd5c93dbf3f9cf19825d5262f9c1c686
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-07-07 08:59:53 +0530

    feat/yielding a dictionary and outputing it into a json file

--- postscrap/postscrap/spiders/post_spider.py
+++ postscrap/postscrap/spiders/post_spider.py
@@ -6,14 +6,15 @@ class PostSpider(scrapy.Spider):
     name = 'posts'
 
     start_urls = [
-        'https://blog.scrapinghub.com/page/1/',
-        'https://blog.scrapinghub.com/page/2/'
+        'https://blog.scrapinghub.com/page/1/'
     ]
     # default callback to process donwload responses when their
     # request dont specifies callback
 
     def parse(self, response):
-        page = response.url.split('/')[-1]
-        filename = 'posts-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        for post in response.css('div.post-item'):
+            yield{
+                'title': post.css('.post-header h2 a::text')[0].get(),
+                'date': post.css('.post-header a::text')[1].get(),
+                'author': post.css('.post-header a::text')[2].get()
+            }
