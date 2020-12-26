7af1e046190c3acc484624eb4bad71fadfbd4911
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-12-26 14:12:27 +0530

    feat/calling another function

--- stock_analysis/api_method.py
+++ stock_analysis/api_method.py
@@ -3,11 +3,11 @@ import json
 from datetime import date
 
 
-def get_stock_fundamental(ticker, report_type):
+def get_stock_data(ticker, endpoint, report_type):
     today = date.today().strftime('%Y-%m-%d')
     # ticker = 'IOC'
     # report_type = 'income-statement'
-    url = f'https://morningstar1.p.rapidapi.com/fundamentals/yearly/{report_type}/restated'
+    url = f'https://morningstar1.p.rapidapi.com/{endpoint}/{report_type}'
 
     querystring = {"Ticker": ticker,"Mic":"XNSE"}
 
@@ -25,7 +25,35 @@ def get_stock_fundamental(ticker, report_type):
     else:
         print(response.status_code)
 
-    path = "C:\\Users\\panka\\github_repo\\datasets\\share"
-    path_to_file = f'{path}\\{ticker}_{report_type}_{today}.json'
+    path = "C:\\Users\\panka\\github_repo\\share"
+    report_name = report_type.split('/')[0]
+    path_to_file = f'{path}\\{ticker}_{report_name}_{today}.json'
     with open(path_to_file, 'w') as f:
         f.write(json_object)
+
+def get_general_data(endpoint, report_type):
+    today = date.today().strftime('%Y-%m-%d')
+    url = f'https://morningstar1.p.rapidapi.com/{endpoint}/{report_type}'
+
+    querystring = {"Mic":"XNSE"}
+
+    headers = {
+        'accept': "string",
+        'x-rapidapi-key': "5015d61e13mshcbb3771deca7b37p19c7c8jsnc900286d0d7b",
+        'x-rapidapi-host': "morningstar1.p.rapidapi.com"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    if response.status_code == 200:
+        # get the response data in readable format
+        json_object = json.dumps(response.json(), sort_keys=True, indent=4)
+    else:
+        print(response.status_code)
+
+    path = "C:\\Users\\panka\\github_repo\\share"
+    report_name = report_type.split('/')[0]
+    path_to_file = f'{path}\\{report_name}_{today}.json'
+    with open(path_to_file, 'w') as f:
+        f.write(json_object)
+
