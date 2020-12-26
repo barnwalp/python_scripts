92250e001317f692d96d95a624ec2c64845e9590
Pankaj Barnwal <suno.pankaj@gmail.com>
2020-12-26 14:12:54 +0530

    feat/second function for getting general data

--- stock_analysis/stock_main.py
+++ stock_analysis/stock_main.py
@@ -1,9 +1,13 @@
 from working_with_api import *
 
 
-# There are three type of fundamental reports:
-# 1. balance-sheet
-# 2. income-statement
-# 3. cashflow-statement
-# all these reports are downloaded in yearly basis
-get_stock_fundamental('IOC', 'balance-sheet')
+# Type of fundamental reports (3rd argument of the function):
+# 1. balance-sheet/restated
+# 2. income-statement/restated
+# 3. cashflow-statement/restated
+# 4. statistics
+
+# Type of endpoints (2nd argument of the function)
+# 1. fundamentals/yearly
+# 2. keyratio
+get_stock_data('IOC', 'fundamentals/yearly', 'balance-sheet/restated')
