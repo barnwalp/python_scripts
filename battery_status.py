f515b2d6aa53abbbec30204287c9fe83b03b608c
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-01-06 08:26:35 +0530

    feat/checkling charger plugged status

--- battery_status.py
+++ battery_status.py
@@ -8,8 +8,8 @@ def message_box(title, text, style):
 
 battery = psutil.sensors_battery()
 
-if battery.percent < 25:
+if battery.percent < 35 and not battery.power_plugged:
     message_box(f'Battery is below 25% capacity', 'Plug your charger', 1)
-elif battery.percent > 85:
+elif battery.percent > 85 and battery.power_plugged:
     message_box(f'Battery is above 85% capacity', 'UnPlug your charger', 1)
 
