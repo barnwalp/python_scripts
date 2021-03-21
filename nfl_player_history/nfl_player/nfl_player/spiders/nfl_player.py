90aceb74c57cab5a4ff25b9d4cab1a6e5aaa515b
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-03-21 15:46:52 +0530

    feat/cleaning the output data

--- nfl_player_history/nfl_player/nfl_player/spiders/nfl_player.py
+++ nfl_player_history/nfl_player/nfl_player/spiders/nfl_player.py
@@ -12,11 +12,20 @@ class NflSpider(scrapy.Spider):
     """
 
     def parse(self, response):
-        for row in response.css('.d3-o-table--horizontal-scroll table'):
-            yield{
-                'player': row.xpath("//tr//td[1]//div//a//text()").get(),
-                'team': row.xpath("//tr//td[2]//text()").get()
-            }
+        row = response.css('.d3-o-table--horizontal-scroll table')
+        items = {
+            'player': row.xpath("//tr//td[1]//div//a//text()").getall(),
+            'team': row.xpath("//tr//td[2]//text()").getall()
+        }
+
+        # Removing extra white spaces and newline characters from items
+        data = dict()
+        for key, value in items.items():
+            li = []
+            for val in value:
+                li.append(val.strip())
+            data[key] = li
+        yield data
         next_page = response.css(
             '.nfl-o-table-pagination__buttons a::attr(href)').get()
         if next_page is not None:
