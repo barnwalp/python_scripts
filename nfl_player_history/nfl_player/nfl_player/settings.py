43b3acd6df590867a1108bcb1c93887e175711e4
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-03-22 08:15:52 +0530

    feat/adding middleware for rotating user-agent

--- nfl_player_history/nfl_player/nfl_player/settings.py
+++ nfl_player_history/nfl_player/nfl_player/settings.py
@@ -48,6 +48,15 @@ ROBOTSTXT_OBEY = True
 #    'nfl_player.middlewares.NflPlayerSpiderMiddleware': 543,
 #}
 
+# scrapy-user-agents uses a file with 2200 user-agent strings collected from 
+# <https://developers.whatismybrowser.com/> using 
+# <https://github.com/hyan15/crawler-demo/tree/master/crawling-basic/common_user_agents>. 
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
+
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
