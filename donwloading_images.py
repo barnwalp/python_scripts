6641ed40e70cf11a7b74cff858c47cbfe0da66d7
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-04-03 12:45:27 +0530

    feat/scrapping ntschool using request

--- donwloading_images.py
+++ donwloading_images.py
@@ -1,6 +1,6 @@
 import requests
 
-
+"""
 url = "https://unsplash.com/napi/search/photos?query=motivational%20quotes&per_page=50&page=5&xp="
 r = requests.get(url)
 data = r.json()
@@ -10,3 +10,21 @@ for item in data['results']:
     urls = item['urls']['regular']
     with open(name+".jpg", "wb") as f:
         f.write(requests.get(urls).content)
+"""
+
+url = "https://directory.ntschools.net/api/System/GetAllSchools"
+r = requests.get(url)
+data = r.json()
+# print(data)
+
+for item in data:
+    school_name = item['schoolName']
+    school_type = item['schoolType']
+    area = item['electorate']
+    region = item['decsRegion']
+    government_school = item['isGovernment']
+    code = item['itSchoolCode']
+    pre_school = item['isPreSchool']
+    # newline character can't beol']
+    nl = '\n'
+    print(f'{school_name}{nl}')
