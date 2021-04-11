c0461e08ee2fc27aed89e71785d1b86929f48251
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-04-11 09:31:47 +0530

    feat/calculating vehicle revenue

--- work/transport_main.py
+++ work/transport_main.py
@@ -1,62 +1,44 @@
 import pandas as pd
-from transport_methods import *
+from transport_methods import per_trip_cost
+from transport_methods import fixed_cost
 import os
 from openpyxl import load_workbook
 import openpyxl
 
 
-os.chdir('D:\Personal\Reports')
+os.chdir('D:\\Personal\\Reports')
 # print(os.listdir())
 df = pd.read_excel('Transport Calculation.xlsx', sheet_name='Vehicle')
 
-
 vehicle_dict = df[['Vehicle Number', 'TT Details']].set_index('Vehicle Number').T.to_dict('list')
-
 # we can remove nan key in dictionary by simple list comprehension based on
 # on the fact that nan does not equal itself
 vehicle_dict = {k: v for k, v in vehicle_dict.items() if k == k}
-for key, value in vehicle_dict.items():
-    monthly_fixed_cost = fixed_cost(value[0])/12
-    vehicle_dict[key].append(monthly_fixed_cost)
-
-# print(vehicle_dict)
 
 trip_dict = df[['Unique Id', 'Vehicle Number', 'RO Name', 'Owners Share', 'RTKM', 'TT Details', 'No of Trip']].set_index('Unique Id').T.to_dict('list')
-# print(trip_dict)
-rtkm_rate = {14: 2.85, 22: 2.44}
+vehicle_data = {14: [2.85], 22: [2.44]}
+
+for key, value in vehicle_data.items():
+    monthly_fixed_cost = fixed_cost(key)/12
+    vehicle_data[key].append(monthly_fixed_cost)
 
 for key, value in trip_dict.items():
     per_trip_expense = per_trip_cost(value[3], value[4], value[5])
-    revenue_per_month = value[3] * value[4] * rtkm_rate[value[4]] * value[5]
+    revenue_per_month = value[3] * value[4] * vehicle_data[value[4]][0] * value[5]
     trip_dict[key].append((revenue_per_month))
     trip_dict[key].append(per_trip_expense)
 
-print(trip_dict)
-"""
-net_profit = []
-own_profit = []
-percent_share = []
-nl = "\n"
-for index, row in df.iterrows():
-    # print(f'{index}-->{row["Owners Share"]}-->{row["RTKM"]}-->{row["TT Details"]}-->{row["No of Trip"]}{nl}')
-    net_profit.append(revenue_share(row["Owners Share"], row["RTKM"], row["TT Details"], row["No of Trip"])[0])
-    own_profit.append(revenue_share(row["Owners Share"], row["RTKM"], row["TT Details"], row["No of Trip"])[1])
-    percent_share.append(revenue_share(row["Owners Share"], row["RTKM"], row["TT Details"], row["No of Trip"])[2])
-    # print(f'{net_profit}-->{own_profit}-->{percent_share}')
+print(trip_dict['1-208483'][0])
+for key1, value1 in vehicle_dict.items():
+    profit = 0
+    for key2, value2 in trip_dict.items():
+        if value2[0] == key1:
+            profit = profit + value2[-2] - value2[-1]
+    vehicle_dict[key1].append(profit)
+    vehicle_dict[key1].append(profit*0.4)
 
-# print(f'net_profit is: {net_profit}{nl}')
-# print(f'own_profit is: {own_profit}{nl}')
-# print(f'percentage_share is: {percent_share}{nl}')
+df_dict = pd.DataFrame.from_dict(vehicle_dict, orient='index')
 
-df['net profit'] = net_profit
-df['own profit'] = own_profit
-df['percentage share'] = percent_share
-
-print(df.head())
-# Function arguments are: owner_share, rtkm, tt_capacity, no_of_trip
-# print(revenue_share(0.08, 1100, 14, 8))
-
-with pd.ExcelWriter('Monthly Calculation - Transport.xlsx', engine='openpyxl') as writer:
-    writer.book = load_workbook('Monthly Calculation - Transport.xlsx')
-    df.to_excel(writer, index=False)
-"""
+with pd.ExcelWriter('Transport Calculation.xlsx', engine='openpyxl') as writer:
+    writer.book = load_workbook('Transport Calculation.xlsx')
+    df_dict.to_excel(writer, index=False)
