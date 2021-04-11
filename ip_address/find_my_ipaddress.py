f3a54805c1fd58f7c505d083a82c64c93fb17af5
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-04-11 22:03:39 +0530

    chores/files required for heroku

--- ip_address/find_my_ipaddress.py
+++ ip_address/find_my_ipaddress.py
@@ -1,7 +1,6 @@
 from requests import get
 import datetime
 import pandas as pd
-import os
 
 
 ip = get('https://api.ipify.org').text
@@ -9,8 +8,8 @@ ip = get('https://api.ipify.org').text
 now = datetime.datetime.now()
 current_time = now.strftime('%Y-%m-%d %H:%M')
 
-df = pd.DataFrame(data = [[current_time, ip]])
+df = pd.DataFrame(data=[[current_time, ip]])
 
 # Running this script in ubuntu workbench
-os.chdir('/home/ubuntu/Desktop')
+# os.chdir('/home/ubuntu/Desktop')
 df.to_csv('ip_address.csv', mode='a', header=False, index=False)
