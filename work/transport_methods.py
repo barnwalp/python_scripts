efff5efa15fb47ca5922fd8501a688f0df59aeb1
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-04-11 12:29:09 +0530

    feat/rearranged owner share

--- work/transport_methods.py
+++ work/transport_methods.py
@@ -33,15 +33,14 @@ def revenue_share(owner_share, rtkm, tt_capacity, no_of_trip):
     }
     tt_dict = tt_details[tt_capacity]
     revenue_per_month = rtkm * tt_capacity * tt_dict['rtkm_rate'] * no_of_trip
-    owner_value = revenue_per_month * owner_share
 
-    per_trip_expense = per_trip_cost(rtkm, tt_capacity, no_of_trip)
+    per_trip_expense = per_trip_cost(rtkm, tt_capacity, no_of_trip, owner_share)
     fixed_monthly_expense = fixed_cost(tt_capacity)
 
-    net_profit = revenue_per_month - fixed_monthly_expense - per_trip_expense - owner_value
+    net_profit = revenue_per_month - fixed_monthly_expense - per_trip_expense
     own_profit = net_profit * 0.4
     percentage_share = own_profit / revenue_per_month
-    print(f'{revenue_per_month} --> {fixed_monthly_expense} --> {per_trip_expense} --> {owner_value}')
+    # print(f'{revenue_per_month} --> {fixed_monthly_expense} --> {per_trip_expense}')
     return net_profit, own_profit, percentage_share
 
 
@@ -79,11 +78,11 @@ def fixed_cost(tt_capacity):
         },
     }
     tt_dict = tt_details[tt_capacity]
-    fixed_monthly_expense = tt_dict['emi_7_monthly'] + (tt_dict['driver_helper_salary']*12 + tt_dict['annual_tt_insurance'] + tt_dict['annual_product_insurance'] + tt_dict['annual_road_tax'] + tt_dict['annual_road_permit'] + tt_dict['tyre_yearly_cost'] + tt_dict['lub_spare_yearly_cost']) / 12
+    fixed_monthly_expense = tt_dict['emi_7_monthly'] + ((tt_dict['driver_helper_salary']*12 + tt_dict['annual_tt_insurance'] + tt_dict['annual_product_insurance'] + tt_dict['annual_road_tax'] + tt_dict['annual_road_permit'] + tt_dict['tyre_yearly_cost'] + tt_dict['lub_spare_yearly_cost']) / 12)
     return fixed_monthly_expense
 
 
-def per_trip_cost(rtkm, tt_capacity, no_of_trip):
+def per_trip_cost(rtkm, tt_capacity, no_of_trip, owner_share):
     hsd_rate = 88.69
     tt_details = {
         22: {
@@ -121,10 +120,10 @@ def per_trip_cost(rtkm, tt_capacity, no_of_trip):
 
     revenue_per_month = rtkm * tt_capacity * tt_dict['rtkm_rate'] * no_of_trip
     tds = revenue_per_month * 0.02
+    owner_value = revenue_per_month * owner_share
 
     fuel_cost = (rtkm/tt_dict['mileage']) * hsd_rate
-
-    trip_expense = (tt_dict['toll_per_trip'] + tt_dict['per_trip_expense'] + tt_dict['per_trip_fooding'] + fuel_cost) * no_of_trip + tds
+    trip_expense = (tt_dict['toll_per_trip'] + tt_dict['per_trip_expense'] + tt_dict['per_trip_fooding'] + fuel_cost) * no_of_trip + tds + owner_value
     return trip_expense
 
 
@@ -132,4 +131,4 @@ if __name__ == '__main__':
     print('owner share is 8%')
     print(revenue_share(0.08, 1100, 22, 8)[0])
     print('owner share is 0%')
-    print(revenue_share(0.00, 1100, 14, 7)[1])
+    print(revenue_share(0.05, 916, 14, 7)[1])
