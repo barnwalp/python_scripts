9f600d748665daf1d5128d1478ffaa0a86ed4436
Pankaj Barnwal <suno.pankaj@gmail.com>
2021-05-08 18:01:08 +0530

    feat/removed utf decoding error

--- work/check_inbox.py
+++ work/check_inbox.py
@@ -3,6 +3,7 @@ import email
 import os
 
 
+# Hotmail configuration using imap protocol
 def connect():
     host = 'imap.outlook.com'
     username = os.environ.get('hotmail_email')
@@ -13,33 +14,34 @@ def connect():
     mail.login(username, password)
     return mail
 
-def filter_sender(mail):
+
+def filter_sender(mail, sender):
     mail.select('inbox')
-    # all uid will be returned as byte string
+    # all uid will be returned as byte string, result = OK
+    # and data = [b'5 20 21 25 27 29 31 33 35 37]
     result, data = mail.uid('search', None, 'ALL')
 
     # convert strings to a list
     inbox_item_list = data[0].split()
-    most_recent = inbox_item_list[-1]
-    oldest = inbox_item_list[0]
-
-    # fetching the email data in bytes
-    result2, email_data = mail.uid('fetch', oldest, '(RFC822)')
-
-    # converting byte email to raw_email data
-    raw_email = email_data[0][1].decode('utf-8')
-    # converting raw_email string to email message
-    email_message = email.message_from_string(raw_email)
+    for value in inbox_item_list:
+        # fetching the email data in bytes
+        result2, email_data = mail.uid('fetch', value, '(RFC822)')
+        # converting byte email to raw_email data
+        raw_email = email_data[0][1].decode('utf-8', errors='ignore')
+        # converting raw_email string to email message
+        email_message = email.message_from_string(raw_email)
+        if email_message["From"] == sender:
+            print(f'Email subject: {email_message["Subject"]}')
 
     # method available with email message
     # print(dir(email_message))
-    nl = '\n'
-    print(f'Email is From: {email_message["From"]}')
-    print(f'Email is send to: {email_message["To"]}')
-    print(f'Email subject is: {email_message["Subject"]}')
+    # print(f'Email is From: {email_message["From"]}')
+    # print(f'Email is send to: {email_message["To"]}')
+    # print(f'Email subject is: {email_message["Subject"]}')
 
 
-filter_sender(connect())
+sender = 'vyapar123@outlook.com'
+filter_sender(connect(), sender)
 
 # get content type of email body
 # print(email_message.get_payload())
